3df64b1873b926abf9d00c0757e331a3521983e2
@LuisArana <disconluis@gmail.com>
2021-08-03 09:36:00 +0200

    added user viewes

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php 
+  include_once("controller/index.php");
+?>
+
+<!DOCTYPE html>
+<html lang="es-ES">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+  <title>Frases favoritas</title>
+</head>
+
+<body>
+<main>
+  <?php
+    if($_SESSION["auth"]){
+      if($_SESSION["isAdmin"])
+        include_once("components/section_admin.php");
+      else 
+        include_once("components/section_users.php");
+    } elseif(!$baneado) {
+      include_once("components/form_login.php");
+    } else {
+      include_once("components/section_ban.php");
+    }   
+  ?>
+  </main>
+</body>
+
+</html>
